e15b6ff7d9fa9b648517561e85c2a5890d99c0eb
XYZ <>
2019-12-31 11:08:10 +0800

    添加轮播

--- Android/app/src/main/java/com/example/android/FaXianActivity.java
+++ Android/app/src/main/java/com/example/android/FaXianActivity.java
@@ -1,5 +1,105 @@
 package com.example.android;
 
-public class FaXianActivity {
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class FaXianActivity extends AppCompatActivity implements OnBannerListener {
+    private Banner banner;
+   // private ArrayList<String> list_path;
+    private ArrayList<Integer> list_path = new ArrayList<>();
+    private ArrayList<String> list_title;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faxian);
+        initView();
+    }
+    private void initView() {
+        banner = (Banner) findViewById(R.id.banner_faxian);
+        //放图片地址的集合
+        list_path = new ArrayList<>();
+        //放标题的集合
+        list_title = new ArrayList<>();
+        //获取本地的图片
+        for (int position =1; position <5; position++) {
+            //这里面的a  代表drawable里面自己放的本地图片
+            list_path.add(getResId("banner" + position, R.drawable.class));
+        }
+
+//        list_path.add(R.drawable.banner1);
+//        list_path.add(R.drawable.banner2);
+//        list_path.add(R.drawable.banner3);
+//        list_path.add(R.drawable.banner4);
+        list_title.add("好好学习");
+        list_title.add("天天向上");
+        list_title.add("热爱劳动");
+        list_title.add("不搞对象");
+        //设置内置样式，共有六种可以点入方法内逐一体验使用。
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
+        //设置图片加载器，图片加载器在下方
+        banner.setImageLoader(new MyLoader());
+        //设置图片网址或地址的集合
+        banner.setImages(list_path);
+        //设置轮播的动画效果，内含多种特效，可点入方法内查找后内逐一体验
+        banner.setBannerAnimation(Transformer.Default);
+        //设置轮播图的标题集合
+        banner.setBannerTitles(list_title);
+        //设置轮播间隔时间
+        banner.setDelayTime(3000);
+        //设置是否为自动轮播，默认是“是”。
+        banner.isAutoPlay(true);
+        //设置指示器的位置，小点点，左中右。
+        banner.setIndicatorGravity(BannerConfig.CENTER)
+                //以上内容都可写成链式布局，这是轮播图的监听。比较重要。方法在下面。
+                .setOnBannerListener(this)
+                //必须最后调用的方法，启动轮播图。
+                .start();
+
+
+    }
+    /**
+     * 通过文件名获取资源id 例子：getResId("icon", R.drawable.class);
+     *
+     * @param variableName
+     * @param c
+     * @return
+     */
+    public static int getResId(String variableName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(variableName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+    //轮播图的监听方法
+    @Override
+    public void OnBannerClick(int position) {
+        Log.i("tag", "你点了第"+position+"张轮播图");
+    }
+    //自定义的图片加载器
+    private class MyLoader extends ImageLoader {
+        @Override
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            Glide.with(context.getApplicationContext())
+                    .load(path)
+                    .into(imageView);
+
+        }
+    }
 }

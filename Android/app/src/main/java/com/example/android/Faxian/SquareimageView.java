cf879b6d1c725aada54bfdfd853129ebc45fbc6e
XYZ <>
2020-01-01 18:42:51 +0800

    修改

---
+++ Android/app/src/main/java/com/example/android/Faxian/SquareimageView.java
@@ -0,0 +1,28 @@
+package com.example.android.Faxian;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+
+//public class SquareimageView extends android.support.v7.widget.AppCompatImageView {
+//
+//    public SquareImageView(Context context) {
+//        this(context,null);
+//    }
+//
+//    public SquareImageView(Context context, @Nullable AttributeSet attrs) {
+//        this(context, attrs,0);
+//    }
+//
+//    public SquareImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+//        super(context, attrs, defStyleAttr);
+//
+//        setScaleType(ScaleType.FIT_XY);
+//    }
+//
+//    @Override
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+//    }
+//}
